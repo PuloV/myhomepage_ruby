94385777dce8c15aafaee8e1ea20f136603f2eac
yordan pulov <yordanpulov@gmail.com>
2014-01-24 21:13:10 +0200

    add some relations

--- user_class.rb
+++ user_class.rb
@@ -1,17 +1,7 @@
-require 'sinatra'
-require "rubygems"
-require 'active_record'
-require "awesome_print"
-ActiveRecord::Base.establish_connection ({
-  :adapter => "mysql",
-  :host => "127.0.0.1",
-  :username => "root",
-  :password => "pass",
-  :database =>"myhomepage_ruby"
-})
+require './require.rb'
 
 class Users < ActiveRecord::Base
-  belongs_to :user
+  has_many :links , through: :user_has_links
   def self.make(mail="user",password="pass")
     existing_user = Users.where(:user_email => mail)
     pass = Digest::MD5.hexdigest password.reverse
@@ -20,10 +10,12 @@ class Users < ActiveRecord::Base
 
   def self.login(mail="user",password="pass")
     pass = Digest::MD5.hexdigest password.reverse
-    current_user = Users.where(:user_email => mail , :user_password => pass )
-    ap current_user.where_values_hash
-    if current_user.size
-      return current_user.where_values_hash
+    current_user = Users.find_by(user_email: mail , :user_password => pass )
+    ap "\n====\n"
+    ap current_user
+    ap "\n====\n"
+    if current_user
+      return current_user
     else
      return false
     end
