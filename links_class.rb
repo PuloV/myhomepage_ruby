eed418b949700b29cf5ce8f77aa816b90eed9280
yordan pulov <yordanpulov@gmail.com>
2014-01-24 22:22:37 +0200

    add make links and some relations

--- links_class.rb
+++ links_class.rb
@@ -1,13 +1,19 @@
 require './require.rb'
 
 class Links < ActiveRecord::Base
-  has_many :user_has_links , through: :user_has_links
+  has_many :users_has_links
+
   def self.make(link="http://")
-     unless Links.where(:link_source => link ).first
-     new_link = Links.create(:link_source => link )
-     new_link.users_has_links.create(:users_has_links_name => rand(10..20) , users_ID => 1)
-     p new_link.link_ID if new_link
-     end
+    unless Links.where(:link_source => link ).first
+      new_link = Links.create(:link_source => link )
+      Users_has_links.create({:users_has_links_name => rand(10..20).to_s , :user_ID => 1 , :link_ID => new_link.link_ID})
+      #self.users_has_links.create({:users_has_links_name => rand(10..20).to_s , :user_ID => 1 , :link_ID => new_link.link_ID})
+    end
+  end
+
+  def self.testing
+    p self.user_has_links.all
+    return self.users_has_links.all
   end
 
 end
