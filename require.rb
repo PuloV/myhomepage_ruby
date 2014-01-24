8592981dcd499a1294f4f9558af9ee9ba8e70192
yordan pulov <yordanpulov@gmail.com>
2014-01-24 21:14:25 +0200

    file containing all requires

---
+++ require.rb
@@ -0,0 +1,18 @@
+
+require "rubygems"
+require 'active_record'
+require './user_class.rb'
+require './links_class.rb'
+set :port, 8080
+
+gem 'mysql'
+
+ActiveRecord::Base.establish_connection ({
+  :adapter => "mysql",
+  :host => "127.0.0.1",
+  :username => "root",
+  :password => "pass",
+  :database =>"myhomepage_ruby"
+})
+
+enable :sessions
