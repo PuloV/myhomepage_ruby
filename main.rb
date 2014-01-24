5aa0fe59a7d2294d6a76cd8cdffb291db6d852f9
yordan pulov <yordanpulov@gmail.com>
2014-01-24 21:15:30 +0200

    Remove requires

--- main.rb
+++ main.rb
@@ -1,40 +1,38 @@
 require 'sinatra'
-require "rubygems"
-require 'active_record'
-require './user_class.rb'
-set :port, 8080
-
-
-gem 'mysql'
-
-test = ActiveRecord::Base.establish_connection ({
-  :adapter => "mysql",
-  :host => "127.0.0.1",
-  :username => "root",
-  :password => "pass",
-  :database =>"myhomepage_ruby"
-})
-
-
-
-enable :sessions
+require './require.rb'
 
 before do
   #redirect to('/user/login') if session["logged"].inspect && request.path != "/user/login" && request.path != "/user/register"
+end
 
-end
-class Users < ActiveRecord::Base
-end
 post '/user/register' do
-  Users.new params[:userreg] , params[:passwordreg]
+  Users.make params[:emailreg] , params[:passwordreg]
   redirect to('/user/login')
+end
+
+post '/user/login' do
+  current_user = Users.login params[:email] , params[:password]
+
+  if current_user
+    session["logged"] = true
+    session["username"] = current_user['user_email']
+    session["user_id"] = current_user['user_id']
+    redirect to('/user/bookmarks')
+  else
+    session["logged"] = false
+    redirect to('/user/register')
+  end
 end
 
 get '/user/:task' do |task|
   erb task.to_sym
 end
 get '/' do
-  test = User.all
-  erb "User № 1 = #{test.size}"
+  test = Users.all
+  numb = rand(1..3)
+  Links.make(numb)
+  erb "User № 1 = #{test.size} <br /> Rand = #{numb}"
+
+
 
 end
