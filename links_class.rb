f8565002e71b270bfd4fb3ccea4250aa0e423698
yordan pulov <yordanpulov@gmail.com>
2014-01-29 22:09:34 +0200

    Change class name

--- links_class.rb
+++ links_class.rb
@@ -1,24 +1,18 @@
 require './require.rb'
 
-class Links < ActiveRecord::Base
-  has_many :users_has_links
+class Link < ActiveRecord::Base
+  has_many :user_links
 
   def self.make(link="http://")
-    unless Links.where(:link_source => link ).first
-      new_link = Links.create(:link_source => link )
-      Users_has_links.create({:users_has_links_name => rand(10..20).to_s , :user_ID => 1 , :link_ID => new_link.link_ID})
-      #self.users_has_links.create({:users_has_links_name => rand(10..20).to_s , :user_ID => 1 , :link_ID => new_link.link_ID})
-    end
+    search_for_link = Link.where(:link_source => link ).first
+    return new_link = Link.create(:link_source => link ) unless search_for_link
+    search_for_link
   end
 
-  def self.testing
-    p self.user_has_links.all
-    return self.users_has_links.all
-  end
 
 end
 
-class Users_has_links < ActiveRecord::Base
+class UserLink < ActiveRecord::Base
   belongs_to :users
   belongs_to :links
 end
