b540f2fa31dce4c189e902a0c9d4198134d23f9f
yordan pulov <yordanpulov@gmail.com>
2014-01-29 22:07:24 +0200

    Change class name , create better assoc. , change make_bookmark

--- user_class.rb
+++ user_class.rb
@@ -1,25 +1,29 @@
 require './require.rb'
 
-class Users < ActiveRecord::Base
-  has_many :links , through: :user_has_links
-  def self.make(mail="user",password="pass")
-    existing_user = Users.where(:user_email => mail)
+class User < ActiveRecord::Base
+  has_many  :user_links
+  def self.register(mail="user",password="pass")
+    existing_user = User.where(:user_email => mail)
     pass = Digest::MD5.hexdigest password.reverse
-    Users.create(:user_email => mail , :user_password => pass )
+    User.create(:user_email => mail , :user_password => pass )
   end
 
   def self.login(mail="user",password="pass")
     pass = Digest::MD5.hexdigest password.reverse
-    current_user = Users.find_by(user_email: mail , :user_password => pass )
-    ap "\n====\n"
-    ap current_user
-    ap "\n====\n"
+    current_user = User.find_by(user_email: mail , :user_password => pass )
+
     if current_user
       return current_user
     else
      return false
     end
+  end
 
+  def make_bookmark(user=0,title="The title",link="http://")
+    user_link = Link.make link
+   # ap self.user_links.methods - Object.methods
+    self.user_links.create({:user_link_name => title , :user_id => user , :link_id => user_link.link_id})
+   # UserLink.create({:user_link_name => title , :user_id => user , :link_id => users_link.link_id})
   end
 
 end
