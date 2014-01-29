57844cb645ac07ffd67c4fde38cab2d41aede1f5
yordan pulov <yordanpulov@gmail.com>
2014-01-29 22:09:11 +0200

    Change class name , simple logout , use class instances

--- main.rb
+++ main.rb
@@ -6,13 +6,13 @@ before do
 end
 
 post '/user/register' do
-  Users.make params[:emailreg] , params[:passwordreg]
+  u = User.register params[:emailreg] , params[:passwordreg]
+  ap u
   redirect to('/user/login')
 end
 
 post '/user/login' do
-  current_user = Users.login params[:email] , params[:password]
-
+  current_user = User.login params[:email] , params[:password]
   if current_user
     session["logged"] = true
     session["username"] = current_user['user_email']
@@ -24,15 +24,24 @@ post '/user/login' do
   end
 end
 
+get '/user/logout' do
+    session["logged"] = false
+    session["username"] = nil
+    session["user_id"] = nil
+    redirect to('/user/login')
+
+end
+
 get '/user/:task' do |task|
   erb task.to_sym
 end
-get '/' do
-  test = Users.all
-  numb = rand(1..3)
-  Links.make(numb)
-  erb "User № 1 = #{test.size} <br /> Rand = #{numb}"
 
+get '/' do
+  u = User.where(:user_id => session["user_id"]).first
+  ap u
+  u.make_bookmark(session["user_id"],rand(1000..100000).to_s,rand(0..1000).to_s)
+  ap u.user_links
+  erb "User № 1 = #{User.all.size} <br /> Rand = #{1} <br /> "
 
 
 end
