1b0f9787e99374897ecfcc46fd92177458746aa0
yordan pulov <yordanpulov@gmail.com>
2014-02-02 19:47:53 +0200

    Add colors for bookmarks

--- require.rb
+++ require.rb
@@ -16,3 +16,16 @@ ActiveRecord::Base.establish_connection ({
 })
 
 enable :sessions
+
+$colour = {
+  0 => "tile-blue" ,
+  1 => "tile-green",
+  2 => "tile-red",
+  3 => "tile-yellow",
+  4 => "tile-orange",
+  5 => "tile-pink",
+  6 => "tile-purple",
+  7 => "tile-lime",
+  8 => "tile-magenta",
+  9 => "tile-teal"
+   )
