b30bee7f31937b10a1e1f06c195608d5dc2c144c
yordan pulov <yordanpulov@gmail.com>
2014-02-02 20:38:07 +0200

    Contains array id =>  colors

---
+++ constants.rb
@@ -0,0 +1,14 @@
+module Constants
+COLORS = {
+  0 => "tile-blue" ,
+  1 => "tile-green",
+  2 => "tile-red",
+  3 => "tile-yellow",
+  4 => "tile-orange",
+  5 => "tile-pink",
+  6 => "tile-purple",
+  7 => "tile-lime",
+  8 => "tile-magenta",
+  9 => "tile-teal"
+   }
+end
