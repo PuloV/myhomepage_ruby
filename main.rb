88cad285f4c9cabbb6c3ad73ba7f5695f9852c0e
yordan pulov <yordanpulov@gmail.com>
2014-02-04 01:23:02 +0200

    Devide the main file to few small file witch have their logic (separating different logics)

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require './require.rb'
-
+require './user.rb'
+require './bookmarks.rb'
 before do
   @user_menu = false
   @user_menu = Menu.where(:menu_type => 1) if session["logged"]
@@ -8,110 +9,10 @@ before do
  # redirect to('../user/login') if session["logged"] != true  || ( request.path != "/user/login" || request.path != "/user/register")
 end
 
-post '/user/register' do
-  u = User.register params[:emailreg] , params[:passwordreg]
-  redirect to('../user/login')
-end
 
-post '/bookmark/add/:id' do |id|
-  user = User.where(:user_id => session["user_id"]).first
-  user.make_bookmark user.user_id , params[:title] , params[:url]
-  redirect to('../bookmarks')
-end
 
-post '/bookmark/edit/:id' do |id|
-  user = User.where(:user_id => session["user_id"]).first
-  user_link = user.user_links.where(:link_id => id ).first
-  new_link = Link.make params[:url]
-  con = Mysql.new('127.0.0.1', 'root', 'pass', 'myhomepage_ruby')
-  rs = con.query("UPDATE `myhomepage_ruby`.`user_links` SET `user_link_name` = '#{params[:title]}' , `link_id` = '#{new_link.link_id}' WHERE `user_links`.`user_id` = #{session["user_id"]} AND `user_links`.`link_id` =#{id}")
-  redirect to('../bookmarks')
-end
 
-get '/bookmark/:action/:id' do |action,id|
-  if id != 0 and action !="add"
-    link_source = Link.where(:link_id => id).first.link_source
-    link_title  = UserLink.where(:link_id => id , :user_id => session['user_id']).first.user_link_name
-  else
-    link_source = ""
-    link_title  = ""
-  end
-  erb :bookmark ,:locals => {
-                               :user_menu => @user_menu ,
-                               :top_menu => @top_layer_menu,
-                               :send_rq_to => action,
-                               :link_id => id ,
-                               :link_source => link_source ,
-                               :link_title => link_title
-                             }
-end
 
-post '/user/login' do
-  current_user = params[:email]  ?  (User.login params[:email] , params[:password]) : (User.login params[:emailtop] , params[:passwordtop])
-  if current_user
-    session["logged"] = true
-    session["username"] = current_user['user_email']
-    session["user_id"] = current_user['user_id']
-    redirect to('../bookmarks')
-  else
-    session["logged"] = false
-    redirect to('../user/register')
-  end
-end
-
-get '/user/logout' do
-    session["logged"] = false
-    session["username"] = nil
-    session["user_id"] = nil
-    redirect to('../user/login')
-
-end
-
-get '/bookmarks'  do
-  u = User.where(:user_id => session["user_id"]).first
-  links = {}
-  u.user_links.map.with_index { |link , index| links[index] = {
-    :user_link_name => link.user_link_name ,
-    :link_source => u.links[index].link_source ,
-    :link_id => link.link_id
-    } }
-  erb :bookmarks ,:locals => { :links => links  ,
-                               :color => Constants::COLORS ,
-                               :min_links_per_row => Constants::LINKS_PER_ROW ,
-                               :user_menu => @user_menu ,
-                               :top_menu => @top_layer_menu
-                             }
-
-end
-
-get '/bookmarks/goto/:link' do |link|
-  user_link = Link.where(:link_id => link).first
-  user_link.link_visited += 1
-  user_link.save
-  redirect user_link.link_source
-end
-
-get '/login' do
-    redirect to('../user/login')
-end
-
-get '/user/login'  do
-  erb :login ,:locals => {
-                               :user_menu => @user_menu ,
-                               :top_menu => @top_layer_menu
-                             }
-end
-
-get '/register' do
-    redirect to('../user/register')
-end
-
-get '/user/register'  do
-  erb :register ,:locals => {
-                               :user_menu => @user_menu ,
-                               :top_menu => @top_layer_menu
-                             }
-end
 
 get '/user/:task' do |task|
   erb task.to_sym
---
+++ user.rb
@@ -0,0 +1,47 @@
+post '/user/register' do
+  u = User.register params[:emailreg] , params[:passwordreg]
+  redirect to('../user/login')
+end
+
+post '/user/login' do
+  current_user = params[:email]  ?  (User.login params[:email] , params[:password]) : (User.login params[:emailtop] , params[:passwordtop])
+  if current_user
+    session["logged"] = true
+    session["username"] = current_user['user_email']
+    session["user_id"] = current_user['user_id']
+    redirect to('../bookmarks')
+  else
+    session["logged"] = false
+    redirect to('../user/register')
+  end
+end
+
+get '/user/logout' do
+    session["logged"] = false
+    session["username"] = nil
+    session["user_id"] = nil
+    redirect to('../user/login')
+
+end
+
+get '/login' do
+    redirect to('../user/login')
+end
+
+get '/user/login'  do
+  erb :login ,:locals => {
+                               :user_menu => @user_menu ,
+                               :top_menu => @top_layer_menu
+                             }
+end
+
+get '/register' do
+    redirect to('../user/register')
+end
+
+get '/user/register'  do
+  erb :register ,:locals => {
+                               :user_menu => @user_menu ,
+                               :top_menu => @top_layer_menu
+                             }
+end
