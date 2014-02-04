9935c0590f8616ae9f6fdf705ce4b02ed90c9eed
yordan pulov <yordanpulov@gmail.com>
2014-02-04 17:10:09 +0200

    Add validating user_email

--- user_class.rb
+++ user_class.rb
@@ -3,21 +3,36 @@ require './require.rb'
 class User < ActiveRecord::Base
   has_many  :user_links
   has_many  :links , through: :user_links
+
+  validates_format_of :user_email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/ , :message =>  "Въведете валиден Email !"
+
+  validates :user_email, length: {
+    minimum: 9,
+    maximum: 50,
+   # tokenizer: lambda { |str|  apvalue  str.scan(/\A[^@]+@([^@\.]+\.)+[^@\.]+\z/) ;  str.scan(/\A[^@]+@([^@\.]+\.)+[^@\.]+\z/) },
+    too_short: "Минимума на символи е 9 ! ",
+    too_long: "Надвишили сте максимума който е 50 символа !"
+  }
+
+
   def self.register(mail="user",password="pass")
     existing_user = User.where(:user_email => mail)
     pass = Digest::MD5.hexdigest password.reverse
-    User.create(:user_email => mail , :user_password => pass )
+    user = User.create(:user_email => mail , :user_password => pass )
+    apvalue user.errors
+    return user
   end
 
   def self.login(mail="user",password="pass")
     pass = Digest::MD5.hexdigest password.reverse
     current_user = User.find_by(user_email: mail , :user_password => pass )
 
-    if current_user
-      return current_user
-    else
-     return false
-    end
+    return current_user
+ #   if current_user
+ #    return current_user
+ #   else
+  #   return false
+#    end
   end
 
   def make_bookmark(user=0,title="The title",link="http://")
