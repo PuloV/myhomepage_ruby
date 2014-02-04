f10482142b6e3af4563e7b638e96211f36142b67
yordan pulov <yordanpulov@gmail.com>
2014-02-04 13:56:02 +0200

    Add if user is logged to show user menu instead of login form

--- news.rb
+++ news.rb
@@ -2,6 +2,7 @@ get '/news' do
 	top_news = News.all.limit(3).order(:news_id).reverse_order
   top_news.each{|news| apvalue news}
   erb :news , :locals => {
+                               :user_id => session["user_id"] ,
                                :top_news => top_news ,
                                :user_menu => @user_menu ,
                                :top_menu => @top_layer_menu
