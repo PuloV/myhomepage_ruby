3d297bbc47d3879a6002ddb0880de269e3ce2fe3
yordan pulov <yordanpulov@gmail.com>
2014-02-04 14:14:00 +0200

    Change open to visited in get

--- charts.rb
+++ charts.rb
@@ -1,8 +1,8 @@
 get "/chart" do
-  redirect ("../charts/most_opened")
+  redirect ("../charts/most_visited")
 end
 
-get "/charts/most_opened" do
+get "/charts/most_visited" do
  links = Link.all.limit(10).order(:link_visited).reverse_order
  links.each {|link| apvalue link }
 end
