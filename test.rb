60210c1f8f868ed420cfb169a003bff20926642b
yordan pulov <yordanpulov@gmail.com>
2014-02-07 00:58:38 +0200

    Add require for bookmarks test

--- test.rb
+++ test.rb
@@ -3,5 +3,7 @@ require 'test/unit'
 require 'rack/test'
 require './require.rb'
 require './test/user.rb'
+require './test/bookmark.rb'
 set :environment, :test
-
+enable :sessions
+#apvalue User.all
